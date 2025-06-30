16ac6a71cf2cc89a7323172ce00fd1c621c35d25
ivanncomit <fabrivancj@gmail.com>
2025-06-30 10:52:40 -0300

    ejercicio 1 completado

--- Tarea/Program.cs
+++ Tarea/Program.cs
@@ -1,2 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using EspacioTareas;
+using System.Text.Json;
+
+HttpClient cliente = new HttpClient();
+string url = "https://jsonplaceholder.typicode.com/todos/";
+
+Console.WriteLine("Obteniendo tareas desde la API...");
+string responseJson = await cliente.GetStringAsync(url);
+
+List<Tarea> tareas = JsonSerializer.Deserialize<List<Tarea>>(responseJson);
+
+Console.WriteLine("\n--- TAREAS PENDIENTES ---");
+
+foreach (var tarea in tareas)
+{
+    if (!tarea.completed)
+    {
+        Console.WriteLine($"Título: {tarea.title} | Estado: Pendiente");
+    }
+}
+
+Console.WriteLine("\n--- TAREAS COMPLETADAS ---");
+foreach (var tarea in tareas.FindAll(t => t.completed))
+{
+    if (tarea.completed)
+    {
+        Console.WriteLine($"Título: {tarea.title} | Estado: Completada");
+    }
+}
+
+// Guardar archivo JSON localmente
+string outputJson = JsonSerializer.Serialize(tareas, new JsonSerializerOptions { WriteIndented = true });
+File.WriteAllText("../tareas.json", outputJson);
+Console.WriteLine("\nArchivo 'tareas.json' guardado en el directorio de ejecución.");
---
+++ Tarea/Tarea.cs
@@ -0,0 +1,11 @@
+namespace EspacioTareas{
+
+    public class Tarea
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public bool completed { get; set; }
+    }
+
+}
