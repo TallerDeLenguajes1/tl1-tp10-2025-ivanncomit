118db963eb2964e20cee6c9e199b73ed62000403
ivanncomit <fabrivancj@gmail.com>
2025-06-30 11:14:03 -0300

    ejercicio2 completado

--- Usuarios/Program.cs
+++ Usuarios/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Text.Json;
+using EspacioUsuarios;
+
+HttpClient cliente = new HttpClient();
+string url = "https://jsonplaceholder.typicode.com/users";
+
+Console.WriteLine("Obteniendo tareas desde la API...");
+string responseJson = await cliente.GetStringAsync(url);
+
+
+List<Usuario> usuarios = JsonSerializer.Deserialize<List<Usuario>>(responseJson);
+
+Console.WriteLine("\n--- Primeros 5 usuarios ---");
+
+int contador = 5;
+
+
+foreach (var user in usuarios)
+{
+    if (contador > 0)
+    {
+        Console.WriteLine($"\nNombre: {user.name}\nCorreo: {user.email}\nDireccion: {user.address.city} | {user.address.street} | {user.address.suite}\n");
+        contador--;
+    }
+}
